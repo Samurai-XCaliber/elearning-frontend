dd96d7c0ce873b56a1c000b7586b9840e90b151b
SAMVISWAS <144431404+Samurai-XCaliber@users.noreply.github.com>
2025-03-27 19:34:49 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,36 @@
-import React from "react";
-import "./Header.css";
+import React, { useState } from "react";
+import "./header.css";
 import { Link } from "react-router-dom";
 
-const Header = ({isAuth}) => {
+const Header = ({ isAuth }) => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header>
-        <div className="logo"><img src="../../../public/E learning.svg"/>E-Learning</div>
+      <div className="logo">
+        <img src="../../../public/E learning.svg" alt="Logo" />
+        EDUSEI
+      </div>
 
-        <div className="link">
-            <Link to={'/'}>Home</Link>
-            <Link to={'/courses'}>Courses</Link>
-            <Link to={'/live-classes'}>Live Classes</Link>
-            <Link to={'/about'}>About</Link>
-           {isAuth ? ( 
-            <Link to={'/account'}>Account</Link>
-          ) : ( 
-            <Link to={'/login'}>Login</Link>
-          )}
-        </div>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <div className={`link ${menuActive ? "active" : ""}`}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/courses"}>Courses</Link>
+        <Link to={"/live-classes"}>Live Classes</Link>
+        <Link to={"/about"}>About</Link>
+        {isAuth ? (
+          <Link to={"/account"}>Account</Link>
+        ) : (
+          <Link to={"/login"}>Login</Link>
+        )}
+      </div>
     </header>
   );
 };
